d59222127828f0f95adc3d5c873fcd89092d7d44
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-09-01 22:47:33 +0100

    update packages; fix a few type things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
-
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
